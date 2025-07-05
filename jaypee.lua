b6d66f3d3eb12f16a5482bd9151136b26242dafb
brivince <brivince14@gmail.com>
2025-07-05 09:15:44 +0800

    Update jaypee.lua

--- jaypee.lua
+++ jaypee.lua
@@ -18,7 +18,7 @@ local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:Wait()
 local HRP = Character:WaitForChild("HumanoidRootPart")
 
 --// Config
-local PARTIAL_NAME = "Ostrich [1.77 KG] [Age 2]"  -- Name match (can be partial like "Ostrich")
+local PARTIAL_NAME = "Ostrich [1.93 KG] [Age 2]"  -- Name match (can be partial like "Ostrich")
 local TYPE = "Pet"               -- "Seed" or "Pet"
 local CLONE_EVERY = 3            -- seconds between each duplication
 local MAX_CLONES = 10            -- stop after this many clones
